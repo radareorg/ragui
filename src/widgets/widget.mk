ea4032d69ca7a787ff1310b9708d74fb8090f0bd
pancake <pancake@nopcode.org>
2011-04-19 17:50:03 +0200

    * Add BugDialog widget

---
+++ src/widgets/widget.mk
@@ -0,0 +1,21 @@
+CFILES=$(subst .gtkon,.c,$(GTKONFILES))
+VALAFLAGS= --vapidir=../include --Xcc=-I../include 
+VALAFLAGS+=$(addprefix --pkg ,$(PACKAGES))
+OFILES=$(subst .c,.o,$(CFILES))
+
+all: main
+
+# TODO: use a single gtkamlc line
+lib:
+	# generate the mountswidget library
+	gtkamlc -C --library ${LIBNAME} --vapi=../include/${LIBNAME}.vapi -H ../include/${LIBNAME}.h ${PKG} ${VALAFLAGS} ${GTKONFILES}
+	${CC} ${CFLAGS} -c -I../include `pkg-config --cflags gtk+-2.0 r_core` ${CFILES}
+	rm -f lib${LIBNAME}.a
+	ar -q lib${LIBNAME}.a ${OFILES}
+
+main: lib
+
+clean:
+	rm -f *.c *.a *.o
+
+.PHONY: main lib clean
